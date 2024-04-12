4f743a5d6a96506767c6de89738b73bbcef061bd
ajaypv <1nh19cs705ajaypv@gmail.com>
2024-04-12 19:32:01 +0530

    added images for the projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,12 @@
+
+
 window.onload = function() {
     const mobileMenu = document.getElementById("toggle-menu");
     const mobileMenuIcon = document.getElementById("mobile-menu");
     const mvclosebutton = document.getElementById("mvclosebutton");
+    const showIframeButton = document.getElementById("myImage");
+
+    showIframeButton.addEventListener("click", showIframe);
 
     if (mobileMenu) {
         mobileMenu.addEventListener("click", function() {
@@ -14,5 +19,22 @@ window.onload = function() {
             mobileMenuIcon.classList.toggle("hidden");
         });
     }
+
+    function showIframe() {
+        console.log("Show iframe clicked");
+        var iframe = document.getElementById('myIframe');
+        var iframe2 = document.getElementById('myIframe2');
+
+        iframe.src = "https://www.canva.com/design/DAFE3DzJyVs/view?embed";
+        iframe2.src = "https://www.canva.com/design/DAFULR3s2O0/bFmy5kNRrFaqtLb2BEbqsQ/view?embed";
+        
+        document.getElementById('myImage').style.display = 'none';
+        document.getElementById('myImage2').style.display = 'none';
+
+        iframe.style.display = 'block';
+        iframe2.style.display = 'block';
+    }
+    setTimeout(showIframe, 5000);
 };
+
 
