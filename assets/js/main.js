2c2d6e8f8debf2176a99f19b5a215de01f9ac222
ajaypv <1nh19cs705ajaypv@gmail.com>
2024-04-13 08:35:55 +0530

    optimised the js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,40 @@
-
-
 window.onload = function() {
     const mobileMenu = document.getElementById("toggle-menu");
     const mobileMenuIcon = document.getElementById("mobile-menu");
     const mvclosebutton = document.getElementById("mvclosebutton");
     const showIframeButton = document.getElementById("myImage");
-    const navLinks = document.querySelectorAll('nav a');
+    const navLinks = document.querySelector('nav');
 
     showIframeButton.addEventListener("click", showIframe);
 
     if (mobileMenu) {
-        mobileMenu.addEventListener("click", function() {
-            console.log("Mobile menu toggle clicked");
-            mobileMenuIcon.classList.toggle("hidden");
-        });
-
-        mvclosebutton.addEventListener("click", function() {
-            console.log("Mobile menu close clicked");
-            mobileMenuIcon.classList.toggle("hidden");
-        });
+        mobileMenu.addEventListener("click", toggleMenu);
+        mvclosebutton.addEventListener("click", toggleMenu);
     }
 
-    
+    navLinks.addEventListener('click', handleNavLinkClick);
 
-    navLinks.forEach(link => {
-      link.addEventListener('click', (event) => {
-        console.log("Nav link clicked");
+    function toggleMenu() {
+        console.log("Menu toggle clicked");
         mobileMenuIcon.classList.toggle("hidden");
+    }
 
-        event.preventDefault(); // Prevent default anchor link behavior
-        const targetSection = document.getElementById(link.getAttribute('href').slice(1)); // Get target section ID
-        targetSection.scrollIntoView({ behavior: 'smooth' }); // Scroll smoothly to the section
-      });
-    });
+    function handleNavLinkClick(event) {
+        const link = event.target;
+        if (link.tagName === 'A') {
+            console.log("Nav link clicked");
+            mobileMenuIcon.classList.toggle("hidden");
+
+            event.preventDefault(); // Prevent default anchor link behavior
+            const targetSection = document.getElementById(link.getAttribute('href').slice(1)); // Get target section ID
+            targetSection.scrollIntoView({ behavior: 'smooth' }); // Scroll smoothly to the section
+        }
+    }
 
     function showIframe() {
         console.log("Show iframe clicked");
-        var iframe = document.getElementById('myIframe');
-        var iframe2 = document.getElementById('myIframe2');
+        const iframe = document.getElementById('myIframe');
+        const iframe2 = document.getElementById('myIframe2');
 
         iframe.src = "https://www.canva.com/design/DAFE3DzJyVs/view?embed";
         iframe2.src = "https://www.canva.com/design/DAFULR3s2O0/bFmy5kNRrFaqtLb2BEbqsQ/view?embed";
@@ -50,9 +47,9 @@ window.onload = function() {
     }
 
     function showImages(){
-        var image1 =  document.getElementById('myImage');
-        var image2 =  document.getElementById('myImage2');
-        var profileimage = document.getElementById('profileimage');
+        const image1 =  document.getElementById('myImage');
+        const image2 =  document.getElementById('myImage2');
+        const profileimage = document.getElementById('profileimage');
 
         profileimage.src = "images/aj.webp";
         image1.src = "images/Open-Dots.webp";
@@ -66,5 +63,3 @@ window.onload = function() {
     setTimeout(showIframe, 5000);
     setTimeout(showImages, 1500);
 };
-
-
